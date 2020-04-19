4ac29e9e2597ec5858bef8b389ade100f9d0f8a0
Maria <you@example.com>
2020-04-19 19:22:39 +0700

    init

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello(props) {
+
+  return <div> 
+      <h4>Hello, {props.title}!</h4>
+    </div>
+
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Header from './Header.js';
+import Hello from './Hello.js';
+import Footer from './Footer.js';
+import './style.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Container extends React.Component {
+  constructor() {
+    super();
+    this.state = { 
+      pageTitle: 'Personal page',
+      title: "React",
+      name: 'Alison Wood',
+      phone: "+01 555 757 28 12",
+      email: 'alison_w@gmail.com'
+    }
+  }
+
+  render() { 
+    return <div>
+        <Header page={this.state.pageTitle}/>
+        <Hello title={this.state.title}/>
+        <table class="personal">
+          Проверьте внесенные данные:
+          <table-body>
+            <tr>
+              <td>Ваше имя:</td> 
+              <td class='inp'>{this.state.name}</td> 
+            </tr>
+            <tr>
+              <td>Ваш телефон:</td> 
+              <td class='inp'>{this.state.phone}</td> 
+            </tr>
+            <tr>
+              <td>Ваша электронная почта:</td> 
+              <td class='inp'>{this.state.email}</td> 
+            </tr>
+          </table-body>
+        </table> 
+      <Footer/>
+    </div>
+  };
+}
+
+ReactDOM.render(<Container />, document.getElementById('root'));
